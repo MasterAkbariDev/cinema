adcffe1e67b66ceb5e8c3d2b2dcc03c5ef4c024c
AliAkbar Akbari <masterakbaridev@gmail.com>
2024-04-27 17:18:22 +0330

    third commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Route, Routes } from 'react-router';
 const Home = React.lazy(() => import('./Pages/Home'));
 
 function App() {
 
   return (
-    <Routes>
-      <Route index element={<React.Suspense fallback={<h3>Loading...</h3>}><Home /></React.Suspense>} />
-    </Routes>
+    <React.Suspense fallback={<h3>Loading...</h3>}><Home /></React.Suspense>
   );
 }
 
